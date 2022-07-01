844d1d599c9971851ce242e4fdcc90c02d27faa6
Geon-OZ <tkfkadlekd10@gmail.com>
2022-07-01 20:45:37 +0900

    프로젝트 생성

---
+++ src/main/java/com/finalproject/anyzoo/AnyzooApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.anyzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyzooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyzooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/anyzoo/AnyzooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.anyzoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyzooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
